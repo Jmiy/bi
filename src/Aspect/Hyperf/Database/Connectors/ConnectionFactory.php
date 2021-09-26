c2782fe55cd603d64f4e5f7eabc14a861de2be42
DESKTOP-FL7306K\Administrator <751399695@qq.com>
2021-09-26 16:12:37 +0800

    适配  pgsql

--- src/Aspect/Hyperf/Database/Connectors/ConnectionFactory.php
+++ src/Aspect/Hyperf/Database/Connectors/ConnectionFactory.php
@@ -29,7 +29,7 @@ class ConnectionFactory extends AbstractAspect
     // 要切入的类，可以多个，亦可通过 :: 标识到具体的某个方法，通过 * 可以模糊匹配
     public $classes = [
         HyperfDatabaseConnectionFactory::class . '::createConnector',
-        HyperfDatabaseConnectionFactory::class . '::createConnector',
+        HyperfDatabaseConnectionFactory::class . '::createConnection',
     ];
 
     // 要切入的注解，具体切入的还是使用了这些注解的类，仅可切入类注解和类方法注解
